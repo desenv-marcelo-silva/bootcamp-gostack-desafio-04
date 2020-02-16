7987498f294ac4bbb7fae902fb15b1fc5322b124
desenv-marcelo-silva <desenv.marcelo.silva@gmail.com>
2020-02-16 16:36:50 -0300

    Criação do componente Comments.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import './App.css';
 
 import Header from './components/Header';
+import Comments from './components/Comments';
 
 function App() {
   return (
@@ -13,10 +14,7 @@ function App() {
       <div className="post">
         <h3>Here we're a posts</h3>
         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi atque necessitatibus voluptatum sunt, sit nesciunt delectus alias repellendus iste sequi maiores ducimus id odio facere cum optio consequatur dicta quo.</p>
-        <div className="comments">
-          <h4>Here we're comments</h4>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto veritatis distinctio eaque eius placeat ex debitis perspiciatis officiis ea sunt eligendi at, quod provident nihil modi ad mollitia, ipsum corporis?
-        </div>
+        <Comments />
       </div>
     </div>
     </>
---
+++ src/components/Comments.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Comments() {
+return (
+  <div className="comments">
+    <h4>Here we're comments</h4>
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto veritatis distinctio eaque eius placeat ex debitis perspiciatis officiis ea sunt eligendi at, quod provident nihil modi ad mollitia, ipsum corporis?</p>
+  </div>
+  )
+}
+
+export default Comments;
