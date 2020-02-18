e6a7ec934d40b1240dbcf958215c8679b6121de2
desenv-marcelo-silva <desenv.marcelo.silva@gmail.com>
2020-02-17 21:00:45 -0300

    Separando array fake.

--- src/components/PostLists.js
+++ src/components/PostLists.js
@@ -3,50 +3,16 @@ import React, { Component } from 'react';
 import Posts from './Posts';
 
 import './PostLists.css';
+import fakeState from '../fakeState';
 
 class PostLists extends Component {
   state = {
-    posts: [
-      {
-        id: 1,
-        author: {
-          name: "Julio Alcantara",
-          avatar: "https://randomuser.me/api/portraits/women/82.jpg"
-        },
-        date: "04 Jun 2019",
-        content: "Pessoal, alguém sabe se a Rocketseat está contratando?",
-        comments: [
-          {
-            id: 1,
-            author: {
-              name: "Diego Fernandes",
-              avatar: "https://randomuser.me/api/portraits/women/83.jpg"
-            },
-            content: "Conteúdo do comentário"
-          }
-        ]
-      },
-      {
-        id: 2,
-        author: {
-          name: "Julio Alcantara",
-          avatar: "https://randomuser.me/api/portraits/women/82.jpg"
-        },
-        date: "04 Jun 2019",
-        content: "Pessoal, alguém sabe se a Rocketseat está contratando?",
-        comments: [
-          {
-            id: 1,
-            author: {
-              name: "Diego Fernandes",
-              avatar: "https://randomuser.me/api/portraits/women/83.jpg"
-            },
-            content: "Conteúdo do comentário"
-          }
-        ]
-      },
-    ]
+    posts: []
   };
+
+  componentDidMount() {
+    this.setState({ posts : fakeState })
+  }
 
   render() {
     return (
---
+++ src/fakeState.js
@@ -0,0 +1,40 @@
+export default [
+  {
+    id: 1,
+    author: {
+      name: "Julio Alcantara",
+      avatar: "https://randomuser.me/api/portraits/women/82.jpg"
+    },
+    date: "04 Jun 2019",
+    content: "Pessoal, alguém sabe se a Rocketseat está contratando?",
+    comments: [
+      {
+        id: 1,
+        author: {
+          name: "Diego Fernandes",
+          avatar: "https://randomuser.me/api/portraits/women/83.jpg"
+        },
+        content: "Conteúdo do comentário"
+      }
+    ]
+  },
+  {
+    id: 2,
+    author: {
+      name: "Julio Alcantara",
+      avatar: "https://randomuser.me/api/portraits/women/82.jpg"
+    },
+    date: "04 Jun 2019",
+    content: "Pessoal, alguém sabe se a Rocketseat está contratando?",
+    comments: [
+      {
+        id: 1,
+        author: {
+          name: "Diego Fernandes",
+          avatar: "https://randomuser.me/api/portraits/women/83.jpg"
+        },
+        content: "Conteúdo do comentário"
+      }
+    ]
+  },
+];
